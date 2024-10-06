13297f0d0c1444fdd6885636fb5deb42f39ec423
minket06 <minket06@yahoo.com.tw>
2024-10-06 15:02:49 +0800

    postgresql_insert

--- SQL/Lesson3_4.sql
+++ SQL/Lesson3_4.sql
@@ -4,3 +4,25 @@ Create table student(
 	major varchar(20),
 	PRIMARY KEY(student_id)
 );
+
+select * from student;
+
+--新增資料
+insert into student(name,major)
+values ('小白','歷史')
+
+insert into student(major,name)
+values ('生物','小黑')
+
+insert into student(major,name)
+values (NULL,'小藍')
+
+--新增多筆資料
+insert into student(major,name)
+values ('國文','小綠'),('數學','小綠')
+
+--新增多筆資料，傳回新增的資訊(INSERT INTO RETURNING *)
+insert into student(major,name)
+values ('生物','小線'),('歷史','小線')
+RETURNING *;
+
