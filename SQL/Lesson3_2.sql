7701a4ce325916b3fb50fc216c57e71a26aab096
minket06 <minket06@yahoo.com.tw>
2024-10-06 11:21:23 +0800

    POSTGRESQL_SELECT

---
+++ SQL/Lesson3_2.sql
@@ -0,0 +1,20 @@
+-- SELECT (CODE,NAME)
+-- FROM STATIONS;
+
+-- SELECT * 
+-- FROM STATIONS;
+
+-- SELECT CODE AS 代碼, NAME AS 車站名稱, E_NAME AS 英文名稱
+-- FROM STATIONS ;
+
+SELECT code,name
+FROM stations;
+
+SELECT *
+FROM stations;
+
+SELECT code as 代碼
+FROM stations;
+
+SELECT code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations;
