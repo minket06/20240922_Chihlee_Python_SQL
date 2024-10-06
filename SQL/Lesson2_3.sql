2c5014b0571938ec10500891e480c7a9ead79303
minket06 <minket06@yahoo.com.tw>
2024-10-06 10:55:01 +0800

    alter table name stations

--- SQL/Lesson2_3.sql
+++ SQL/Lesson2_3.sql
@@ -1,8 +1,8 @@
--- Table: public.tr
+-- Table: public.stations
 
--- DROP TABLE IF EXISTS public.tr;
+-- DROP TABLE IF EXISTS public.stations;
 
-CREATE TABLE IF NOT EXISTS public.tr
+CREATE TABLE IF NOT EXISTS public.stations
 (
     id SERIAL ,
     code character varying(50) COLLATE pg_catalog."default",
@@ -10,16 +10,16 @@ CREATE TABLE IF NOT EXISTS public.tr
     e_name character varying(100) COLLATE pg_catalog."default"
 )
 
-ALTER TABLE public.tr  ADD CONSTRAINT tr_pkey PRIMARY KEY (id);
-ALTER TABLE public.tr ADD CONSTRAINT unique_code UNIQUE (code);
-ALTER TABLE public.tr ADD CONSTRAINT unique_name UNIQUE (name);
-ALTER TABLE public.tr ADD CONSTRAINT unique_e_name UNIQUE (e_name);
+ALTER TABLE public.stations  ADD CONSTRAINT tr_pkey PRIMARY KEY (id);
+ALTER TABLE public.stations ADD CONSTRAINT unique_code UNIQUE (code);
+ALTER TABLE public.stations ADD CONSTRAINT unique_name UNIQUE (name);
+ALTER TABLE public.stations ADD CONSTRAINT unique_e_name UNIQUE (e_name);
 
-ALTER TABLE public.tr ALTER COLUMN code SET NOT NULL;
-ALTER TABLE public.tr ALTER COLUMN name SET NOT NULL;
+ALTER TABLE public.stations ALTER COLUMN code SET NOT NULL;
+ALTER TABLE public.stations ALTER COLUMN name SET NOT NULL;
 
-ALTER TABLE public.tr  ALTER COLUMN name TYPE varchar(15);
+ALTER TABLE public.stations  ALTER COLUMN name TYPE varchar(15);
 
 
-ALTER TABLE public.tr DROP CONSTRAINT unique_name;
+ALTER TABLE public.stations DROP CONSTRAINT unique_name;
 
