b9fe896ba75b3a4bc18fd40b9060e04552eb2718
minket06 <minket06@yahoo.com.tw>
2024-10-13 14:35:53 +0800

    lesson4_3_Group_By

--- SQL/lesson4_3_Sam.sql
+++ SQL/lesson4_3_Sam.sql
@@ -1,4 +1,4 @@
-SELECT *
+SELECT (customer_id,staff_id) as G,*
 FROM public.payment
 order by payment_id asc;
 
@@ -20,6 +20,13 @@ select 	customer_id,
 		max(amount) as 最大,
 		min(amount) as 最小
 from payment
+from payment
 group by customer_id
 --order by count(amount) ASC;
 order by 筆數 asc;
+--*********************
+select (first_name || ' ' || last_name) as full_name,sum(amount) as 總和--,* 
+from payment p
+join customer c on p.customer_id=c.customer_id
+group by full_name
+order by 總和 desc
