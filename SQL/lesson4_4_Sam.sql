0cb1c8a8041298bebdb2634d2e1791df1fa6343d
minket06 <minket06@yahoo.com.tw>
2024-10-13 15:30:35 +0800

    lesson4_4

--- SQL/lesson4_4_Sam.sql
+++ SQL/lesson4_4_Sam.sql
@@ -4,4 +4,17 @@ FROM public.payment
 group by staff_id ;
 
 
-select distinct staff_id FROM public.payment
+-- select distinct staff_id FROM public.payment
+/*取出每個員工,在每一個客戶的訂單金額總合*/
+select staff_id,customer_id,sum(amount) as 訂單金額總合
+from public.payment
+group by staff_id,customer_id
+order by customer_id
+
+/*取出每日訂單的總和*/
+-- select payment_date,sum(amount) 
+select payment_date::date as 日期,sum(amount) 
+from payment
+--group by CAST(payment_date as date)
+group by 日期
+order by 日期
