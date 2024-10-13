41f651e594f717b78f1ce63a963b4ded3fa1e950
minket06 <minket06@yahoo.com.tw>
2024-10-13 16:07:13 +0800

    Lesson4_5_SubQuery

--- SQL/lesson4_5_Sam.sql
+++ SQL/lesson4_5_Sam.sql
@@ -14,6 +14,33 @@ from customer
 group by store_id
 having count(customer_id)>300;
 
+--*****
+select * 
+from country
+
+
+select country_id,*
+from country
+where country='Taiwan'
+
+select city 
+from city
+where country_id='92'
+
+--subquery
+select city 
+from city
+where country_id=(
+	select country_id
+	from country
+	where country='Taiwan'
+)
+
+--用JOIN來取代SUBQUERY
+select city
+from city
+join country on country.country_id=city.country_id
+where country='Taiwan'
 
 
 
