842055c029d1e943ce5c025dd7f172e6ef5e945a
minket06 <minket06@yahoo.com.tw>
2024-10-20 10:40:56 +0800

    Lesson5_1_Lesson5_6(基隆火車站2020年,每月份進站人數_2)

--- SQL/lesson5_1.sql
+++ SQL/lesson5_1.sql
@@ -22,3 +22,14 @@ HAVING SUM(gateincomingcnt) >= 5000000
 ORDER BY 總進站人數 DESC;
 
 
+/*基隆火車站2020年,每月份進站人數*/
+SELECT stationname AS 站名, TO_CHAR(date, 'YYYY-MM') AS 日期,SUM(gateincomingcnt) AS 當月總進站人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+/*WHERE date BETWEEN '2022-01-01' AND '2022-12-31';*/
+WHERE EXTRACT(YEAR FROM date) = 2020 AND stationname='基隆'
+GROUP BY 站名,日期;
+
+
+
+
+
