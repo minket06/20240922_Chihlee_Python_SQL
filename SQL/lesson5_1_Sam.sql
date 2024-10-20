d460a7d9b3f1de5abd4c03f4eb294a0baec9d062
minket06 <minket06@yahoo.com.tw>
2024-10-20 10:14:16 +0800

    lesson5_1 modify to lesson5_2

--- SQL/lesson5_1_Sam.sql
+++ SQL/lesson5_1_Sam.sql
@@ -1,12 +1,12 @@
-select * from stations
+select * from stations;
 select *
-from station_in_out 
+from station_in_out ;
 select DATE_PART('year', date) AS year,
 		DATE_PART('month',date) AS month,*
-from station_in_out 
+from station_in_out ;
 
        
---全省各站點2022年進站總人數
+--全省各站點2022年進站總人數(lesson5_1,lesson5_2)
 -- select stacode,sum(gateincomingcnt) 
 -- select *
 select stacode,b.stationname,sum(gateincomingcnt) as total
@@ -15,6 +15,7 @@ left join stations b on a.stacode=b.stationcode
 where date between '2022/01/01' and '2022/12/31' 
 group by stacode,b.stationname
 order by total desc --stacode,b.stationname
+;
 
 -- 全省各站點2022年進站總人數大於5佰萬人的站點
 select stacode,b.stationname,sum(gateincomingcnt)
@@ -24,6 +25,7 @@ where date between '2022/01/01' and '2022/12/31'
 group by stacode,b.stationname
 having sum(gateincomingcnt) >=5000000
 order by stacode
+;
 
 -- 基隆火車站2020年,每月份進站人數
 select stacode,b.stationname,
@@ -34,6 +36,7 @@ left join stations b on a.stacode=b.stationcode
 where date between '2020/01/01' and '2020/12/31' and stacode='900' 
 group by stacode,b.stationname,yearmonth
 order by stacode,b.stationname,yearmonth
+;
 
 -- 基隆火車站2020年,每月份進站人數,由多至少
 select stacode,b.stationname,
@@ -44,7 +47,7 @@ left join stations b on a.stacode=b.stationcode
 where date between '2020/01/01' and '2020/12/31' and stacode='900' 
 group by stacode,b.stationname,yearmonth
 order by total desc
-
+;
 
 -- 基隆火車站2020,2021,2022,每年進站人數
 select stacode,b.stationname,DATE_PART('year',date) as year ,sum(gateincomingcnt)
@@ -53,6 +56,7 @@ left join stations b on a.stacode=b.stationcode
 where date between '2020/01/01' and '2022/12/31'  and stacode='900' 
 group by stacode,b.stationname,year
 order by stacode,b.stationname,year
+;
 
 -- 基隆火車站,臺北火車站2020,2021,2022,每年進站人數
 select stacode,b.stationname,DATE_PART('year',date) as year ,sum(gateincomingcnt)
@@ -61,6 +65,7 @@ left join stations b on a.stacode=b.stationcode
 where date between '2020/01/01' and '2022/12/31'  and stacode in ('900','1000')
 group by stacode,b.stationname,year
 order by stacode,b.stationname,year
+;
 
 -- 請使用SubQuery
 -- 進站人數最多的一筆
@@ -68,6 +73,7 @@ select a.*,b.stationname
 from station_in_out a
 left join stations b on a.stacode=b.stationcode
 where gateincomingcnt in (select max(gateincomingcnt) from station_in_out)
+;
 
 -- 各站點進站人數最多的一筆
 select a.date,a.stacode,b.stationname,a.gateincomingcnt
@@ -75,5 +81,5 @@ from station_in_out a
 left join stations b on a.stacode=b.stationcode
 where (stacode, gateincomingcnt) in (select stacode,max(gateincomingcnt) from station_in_out group by stacode)
 order by stacode, gateincomingcnt
-
+;
 
