0b1e914e8563b948237206e17295ed8db400b856
minket06 <minket06@yahoo.com.tw>
2024-11-17 15:01:41 +0800

    test

--- Lesson9/main_Teacher.py
+++ Lesson9/main_Teacher.py
@@ -1,28 +1,10 @@
 
 from dotenv import load_dotenv
 import streamlit as st
-from time import sleep
-# from datasource_Teacherpy import get_data
-# from tools.datasource import get_data
 from pprint import pprint
-from tools import get_data
-
+import tools
 load_dotenv()
 
-# placeholder = st.empty()
-# with placeholder:
-#     # with st.spinner('下載資料中...'):
-#     #     sleep(5)
-#     with st.spinner('下載資料中...'):
-#         get_data()
-
-
-#     st.success("下載完成",icon=":material/favorite:")
-#     sleep(3)
-
-
-# placeholder.empty()
-    
 
 @st.dialog("目前發生問題,請稍後再試")
 def alert():
@@ -30,11 +12,17 @@ def alert():
         st.stop()
 
 with st.spinner('下載資料中...'):
-#         get_data()
-# alert()
-# st.write('顯示頁面')
-        youbikes = get_data()
+        youbikes:list[dict] = tools.get_data()
+        tools.save_to_database(data=youbikes)
         
 if youbikes is None:
     alert()
 st.write(youbikes)
+
+
+
+    
+
+
+
+    
--- Lesson9/tools/__datasource2__.py
+++ Lesson9/tools/__datasource2__.py
@@ -1,4 +1,9 @@
 import requests
+
+import psycopg2
+import os
+from dotenv import load_dotenv
+
 # 自訂常數
 PI = 3.1415926
 
@@ -25,4 +30,35 @@ def get_data()->list | None:#-> list or None#
 def save_to_database(data:list[dict])->None:#沒有傳出任何東西#
         # pass
         print(data)
-        print("data的資料要放進資料庫")
+        # print("data的資料要放進資料庫")
+        #建立資料表
+        load_dotenv()
+        conn = psycopg2.connect(host=os.environ['HOST_RENDER'],database=os.environ['DATABASE_RENDER'],user=os.environ['USER_RENDER'],password=os.environ['PASSWORD_RENDER'])
+        with conn:
+            with conn.cursor() as cursor:
+                sql = '''
+                CREATE TABLE IF NOT EXISTS 站點資訊(
+                站點編號 VARCHAR(10),
+                站點名稱 VARCHAR(30) NOT NULL,
+                行政區 VARCHAR(10) NOT NULL,
+                站點地址 VARCHAR(50),
+                lat NUMERIC(15,11),
+                lng NUMERIC(15,11),
+                PRIMARY KEY(站點編號)
+                );
+                CREATE TABLE IF NOT EXISTS youbike(
+                日期 TIMESTAMP,
+                編號 VARCHAR(10),
+                總車輛 INTEGER,
+                可借 INTEGER,
+                可還 INTEGER,
+                活動 BOOLEAN,
+                PRIMARY KEY(日期,編號),
+                FOREIGN KEY(編號) REFERENCES 站點資訊(站點編號) 
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+                );
+                '''
+                cursor.execute(sql)    
+                    
+        conn.close()
